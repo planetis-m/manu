28dac2ea7aa4e1b00b2eeab753f28a9e2406dcdf
Your Name <capoiosct@gmail.com>
2018-04-04 17:05:22 +0300

    Updata

---
+++ lu.nim
@@ -0,0 +1,146 @@
+## LU Decomposition.
+##
+## For an m-by-n matrix A with m >= n, the LU decomposition is an m-by-n
+## unit lower triangular matrix L, an n-by-n upper triangular matrix U,
+## and a permutation vector piv of length m so that A(piv,:) = L*U.
+## If m < n, then L is m-by-m and U is m-by-n.
+##
+## The LU decompostion with pivoting always exists, even if the matrix is
+## singular, so the constructor will never fail.  The primary use of the
+## LU decomposition is in the solution of square systems of simultaneous
+## linear equations.  This will fail if isNonsingular() returns false.
+
+type LUDecomposition* = object
+   # Array for internal storage of decomposition.
+   lu: seq[seq[float]]
+   # Row and column dimensions, and pivot sign.
+   m, n, pivsign: int
+   # Internal storage of pivot vector.
+   piv: seq[int]
+
+proc lu*(a: Matrix): LUDecomposition =
+   ## LU Decomposition
+   ## Structure to access L, U and piv.
+   ## param ``a``: Rectangular matrix
+
+   # Use a "left-looking", dot-product, Crout/Doolittle algorithm.
+   result.lu = a.data
+   result.m = a.m
+   result.n = a.n
+   newSeq(result.piv, result.m)
+   for i in 0 ..< result.m:
+      piv[i] = i
+   result.pivsign = 1
+   var lu_rowi: seq[float]
+   var lu_colj = newSeq[float](result.m)
+
+   # Outer loop.
+   for j in 0 ..< result.n:
+
+      # Make a copy of the j-th column to localize references.
+      for i in 0 ..< result.m:
+         lu_colj[i] = result.lu[i][j]
+
+      # Apply previous transformations.
+      for i in 0 ..< result.m:
+         lu_rowi = result.lu[i]
+
+         # Most of the time is spent in the following dot product.
+         let kmax = min(i, j)
+         var s = 0.0
+         for k in 0 ..< kmax:
+            s += lu_rowi[k] * lu_colj[k]
+         lu_rowi[j] -= s
+         lu_colj[i] -= s
+
+      # Find pivot and exchange if necessary.
+      var p = j
+      for i in j + 1 ..< m:
+         if abs(lu_colj[i]) > abs(lu_colj[p]):
+            p = i
+      if p != j:
+         for k in 0 ..< result.n:
+            swap(result.lu[p][k], result.lu[j][k])
+         swap(piv[p], piv[j])
+         result.pivsign = -result.pivsign
+
+      # Compute multipliers.
+      if j < m and result.lu[j][j] != 0.0:
+         for i in j + 1 ..< result.m:
+            result.lu[i][j] /= result.lu[j][j]
+
+proc isNonsingular*(l: LUDecomposition): bool =
+   ## Is the matrix nonsingular?
+   ## returns true if U, and hence A, is nonsingular.
+   for j in 0 ..< n:
+      if l.lu[j][j] == 0:
+         return false
+   return true
+   
+proc getL*(l: LUDecomposition): Matrix =
+   ## Return lower triangular factor
+   result.m = l.m
+   result.n = l.n
+   newData()
+   for i in 0 ..< l.m:
+      for j in 0 ..< l.n:
+         if i > j:
+            result.data[i][j] = l.lu[i][j]
+         elif i == j:
+            result.data[i][j] = 1.0
+
+proc getU*(l: LUDecomposition): Matrix =
+   ## Return upper triangular factor
+   result.m = l.n
+   result.n = l.n
+   newData()
+   for i in 0 ..< l.n
+      for j in 0 ..< l.n:
+         if i <= j:
+            result.data[i][j] = l.lu[i][j]
+
+proc getPivot*(l: LUDecomposition): seq[int] =
+   ## Return pivot permutation vector
+   l.piv
+
+## Return pivot permutation vector as a one-dimensional double array
+proc getFloatPivot*(l: LUDecomposition): seq[float] =
+   result = newSeq[float](l.m)
+   for i in 0 ..< m:
+      result[i] = l.piv[i]
+
+proc det*(l: LUDecomposition) =
+   ## Determinant
+   assert(m == n, "Matrix must be square.")
+   result = pivsign
+   for j in 0 ..< n:
+      result *= l.lu[j][j]
+
+proc solve*(l: LUDecomposition, b: Matrix): Matrix =
+   ## Solve ``A*X = B``
+   ## param ``B``: A Matrix with as many rows as A and any number of columns.
+   ## returns X so that ``L*U*X = B(piv,:)``
+   assert(b.m == l.m, "Matrix row dimensions must agree.")
+   assert(l.isNonsingular, "Matrix is singular.")
+
+   # Copy right hand side with pivoting
+   result.m = l.piv.len
+   result.n = b.n
+   newData()
+   for i in 0 ..< result.m:
+      for j in 0 ..< result.n:
+         result.data[i][j] = m.data[r[i]][j]   
+
+   # Solve L*Y = B(piv,:)
+   for k in 0 ..< n:
+      for i in k + 1 ..< n:
+         for j in 0 ..< nx:
+            result.data[i][j] -= result.data[k][j] * l.lu[i][k]
+
+   # Solve U*X = Y
+   for k in countdow(n - 1, 0):
+      for j in 0 ..< nx:
+         result.data[k][j] /= l.lu[k][k]
+      for i in 0 ..< k:
+         for j in 0 ..< nx:
+            result.data[i][j] -= result.data[k][j] * l.lu[i][k]
--- matrix.nim
+++ matrix.nim
@@ -1,5 +1,4 @@
-import math, random, strutils
-#import cholesky, qr, lu, eigen, svd
+import math
 
 template checkBounds(cond: untyped, msg = "") =
    when compileOption("boundChecks"):
@@ -171,45 +170,6 @@ proc `[]=`*(m: var Matrix, r: Slice[int], c: openarray[int], a: Matrix) =
       for j in 0 ..< c.len:
          m.data[i][c[j]] = a.data[i - r.a][j]
 
-proc t*(m: Matrix): Matrix =
-   ## Matrix transpose
-   result.m = m.n
-   result.n = m.m
-   newData()
-   for i in 0 ..< m.m:
-      for j in 0 ..< m.n:
-         result.data[j][i] = m.data[i][j]
-
-proc norm1*(m: Matrix): float =
-   ## One norm,
-   ## returns maximum column sum.
-   for j in 0 ..< m.n:
-      var s = 0.0
-      for i in 0 ..< m.m:
-         s += abs(m.data[i][j])
-      result = max(result, s)
-
-proc norm2*(m: Matrix): float =
-   ## Two norm,
-   ## returns maximum singular value.
-   svd(m).norm2())
-
-proc normInf*(m: Matrix): float =
-   ## Infinity norm,
-   ## returns maximum row sum.
-   for i in 0 ..< m.m:
-      var s = 0.0
-      for j in 0 ..< m.n:
-         s += abs(m.data[i][j])
-      result = max(result, s)
-
-proc normF*(m: Matrix): float =
-   ## Frobenius norm,
-   ## returns sqrt of sum of squares of all elements.
-   for i in 0 ..< m.m:
-      for j in 0 ..< m.n:
-         result = hypot(result, m.data[i][j])
-
 proc `-`*(m: Matrix): Matrix =
    ## Unary minus
    result.m = m.m
@@ -335,101 +295,3 @@ proc `*`*(a, b: Matrix): Matrix =
          for k in 0 ..< a.n:
             s += a_rowi[k] * b_colj[k]
          result.data[i][j] = s
-
-proc solve(a, b: Matrix): Matrix =
-   ## Solve ``A*X = B``,
-   ## returns solution if A is square, least squares solution otherwise
-   ## parameter ``b``: the right hand side
-   if m == n:
-      lu(a).solve(b)
-   else:
-      qr(a).solve(b)
-
-proc solveTranspose(a, b: Matrix): Matrix =
-   ## Solve ``X*A = B``, which is also ``A'*X' = B'``,
-   ## returns solution if A is square, least squares solution otherwise.
-   ## parameter ``b``: the right hand side
-   t(m).solve(b.t())
-
-proc inverse(m: Matrix): Matrix =
-   ## Matrix inverse or pseudoinverse,
-   ## returns inverse(A) if A is square, pseudoinverse otherwise.
-   solve(m, identity(m.m, m.m))
-
-proc det(m: Matrix): float =
-   ## Matrix determinant
-   lu(m).det()
-
-proc rank(m: Matrix): int =
-   ## Matrix rank,
-   ## returns effective numerical rank, obtained from SVD.
-   svd(m).rank()
-
-proc cond(m: Matrix): float =
-   ## Matrix condition (2 norm),
-   ## returns ratio of largest to smallest singular value.
-   svd(m).cond()
-
-proc trace*(m: Matrix): float =
-   ## Matrix trace,
-   ## returns the sum of the diagonal elements.
-   for i in 0 ..< min(m.m, m.n):
-      result += m.data[i][i]
-
-proc randMatrix*(m, n: int): Matrix =
-   ## Generate matrix with random elements,
-   ## returns an m-by-n matrix with uniformly distributed random elements.
-   result.m = m
-   result.n = n
-   newData()
-   for i in 0 ..< m:
-      for j in 0 ..< n:
-         result.data[i][j] = rand(1.0)
-
-proc identity*(m, n: int): Matrix =
-   ## Generate identity matrix,
-   ## returns An m-by-n matrix with ones on the diagonal and zeros elsewhere.
-   result.m = m
-   result.n = n
-   newData()
-   for i in 0 ..< m:
-      for j in 0 ..< n:
-         if i == j:
-            result.data[i][j] = 1.0
-
-proc columnFormat(s: seq[float]): seq[string] =
-   result = newSeq[string](s.len)
-   for i, v in s:
-      result[i] = formatFloat(v, ffDecimal, 8)
-   var lenLeft = newSeq[int](s.len)
-   var maxLenLeft = 0
-   for i, f in result:
-      let index = f.find('.')
-      lenLeft[i]  = index
-      maxLenLeft = max(maxLenLeft, lenLeft[i])
-   for i in 0 ..< s.len:
-      result[i] = spaces(maxLenLeft  - lenLeft[i]) & result[i]
-
-proc `$`*(m: Matrix): string =
-   var cols: seq[seq[string]]
-   newSeq(cols, m.m)
-   for i in 0 ..< m.m:
-      cols[i] = columnFormat(m.data[i])
-   result = ""
-   for j in 0 ..< m.n:
-      if j == 0:
-         result.add "⎡"
-      elif j == m.n - 1:
-         result.add "⎣"
-      else:
-         result.add "⎢"
-      for i in 0 ..< m.m:
-         if i != 0:
-            result.add "  "
-         result.add cols[i][j]
-      if j == 0:
-         result.add "⎤\n"
-      elif j == m.n - 1:
-         result.add "⎦\n"
-      else:
-         result.add "⎥\n"
