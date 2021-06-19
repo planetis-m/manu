7551ef55c0e44f8d04555086320da8ac328f2e51
planetis-m <b3lievem@yandex.com>
2021-06-19 10:35:05 +0300

    I copied wrong arguments

--- tests/ttimes.nim
+++ tests/ttimes.nim
@@ -1,12 +1,11 @@
-import std/math, manu, utils
+import manu, utils
 
 proc main() =
   var A, T, Z: Matrix[float32]
   var errorCount = 0
   var warningCount = 0
-  let tvals = @[@[1'f32, 2, 3], @[4'f32, 5, 6], @[7'f32, 8, 9], @[10'f32, 11, 12]]
-  T = matrix(tvals)
-  T = A.transpose()
+  let columnwise = @[1'f32, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  A = matrix(columnwise, 3)
   try:
     check(A * 0, Z)
     trySuccess("times(double)...", "")
